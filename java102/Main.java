d4afbbadb1bf580a51cd06f517e2a23132c16ea7
chim418 <chasekim0418@gmail.com>
2024-11-24 13:24:16 -0500

    java-102 : practice:CenterofMass

--- java102/Main.java
+++ java102/Main.java
@@ -1,5 +1,30 @@
 package java102;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Point point = new Point(4, 3);
+        System.out.println("x: " + point.x + " y: " + point.y);
+
+        Point xTranslation = point.translateX(3); // should be (7, 3)
+        Point yTranslation = point.translateY(-7); // should be (4, -4)
+        Point xyTranslation = point.translateX(-4).translateY(-3); // should be (0, 0)
+
+        System.out.println("x transl: " + xTranslation);
+        System.out.println("y transl: " + yTranslation);
+        System.out.println("xy transl: " + xyTranslation);
+
+        System.out.println("point: " + point);
+
+        // Practice: Center of Mass
+        Point[] points = {
+            new Point(2, 3),
+            new Point(4, 5),
+            new Point(6, 7)
+        };
+
+        Point center = Point.centerOfMass(points);
+        System.out.println("Center of mass: x = " + center.x + ", y = " + center.y);
+    }
 }
+    
+    
--- java102/Point.java
+++ java102/Point.java
@@ -3,4 +3,46 @@ package java102;
 public class Point {
     public final double x;
     public final double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point translateX(double t) {
+        return new Point(x + t, y);
+    }
+
+    public Point translateY(double t) {
+        return new Point(x, y +t);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public static double distance(Point p1, Point p2) {
+		return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    }
+
+    // Practice: Center of Mass
+    public static Point centerOfMass(Point[] points) {
+        if (points == null || points.length == 0) {
+            throw new IllegalArgumentException("The points array cannot be null or empty.");
+        }
+        
+        int sumX = 0;
+        int sumY = 0;
+
+        for (Point point : points) {
+            sumX += point.x;
+            sumY += point.y;
+        }
+
+        int centerX = sumX / points.length;
+        int centerY = sumY / points.length;
+
+        return new Point(centerX, centerY);
+    }
 }
